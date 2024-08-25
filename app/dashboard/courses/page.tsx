1b3af2c755495181abbb57e32fcf5b09c03ff70b
Karthi <karthikthee7@gmail.com>
2024-08-25 11:46:44 +0530

    new routes

---
+++ app/dashboard/courses/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Students | Dreamer Academy",
+  description: "Admin dashboard for dreamer academy",
+};
+
+export default function CoursePage() {
+  return <div>Hi</div>;
+}
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "User | Dreamer Academy",
+  description: "Admin dashboard for dreamer academy",
+};
+
+export default function UserPage() {
+  return <div>Hi</div>;
+}
