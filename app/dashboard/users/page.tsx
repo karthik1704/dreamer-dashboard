59b3b12bd7728e038e10ae75d31c00a7eb1524d7
Karthi <karthikthee7@gmail.com>
2024-09-07 18:42:53 +0530

    new changes

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 import UserTable from "./user-table";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "User | Dreamer Academy",
@@ -7,8 +9,20 @@ export const metadata: Metadata = {
 };
 
 export default function UserPage() {
-  return <div>
-
-    <UserTable/>
-  </div>;
+  return (
+    <div className="min-h-svh">
+      <Breadcrumb pageName="Users" />
+      <div className="flex justify-end">
+        <Link
+          href="users/new"
+          className="inline-flex items-center justify-center rounded-md border border-black px-10 py-4 text-center font-medium text-black hover:bg-opacity-90 lg:px-8 xl:px-10"
+        >
+          New User
+        </Link>
+      </div>
+      <div className="flex flex-col gap-10">
+        <UserTable />
+      </div>
+    </div>
+  );
 }
